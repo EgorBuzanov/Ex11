db859cd2bfba1bf9184d431e2043252b103a16da
EgorBuzanov <egor4ek007@gmail.com>
2021-04-05 21:21:32 +0300

    Add tests

--- test/AllTests.cpp
+++ test/AllTests.cpp
@@ -1,8 +1,51 @@
-// Copyright 2020 GHA Test Team
+// Copyright 2020 Egor Buzanov
 
 #include <gtest/gtest.h>
+
+#include <random>
+
+#include "train.h"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
+}
+
+TEST(Train, test1) {
+  Train train;
+  EXPECT_EQ(0, train.getLenght());
+}
+
+TEST(Train, test2) {
+  Train train;
+  std::random_device rd;
+  std::mt19937 mersenne(rd());
+
+  for (size_t i = 0; i < 10; i++) {
+    train.addCage(mersenne() % 2);
+  }
+
+  EXPECT_EQ(10, train.getLenght());
+}
+
+TEST(Train, test3) {
+  Train train;
+  std::random_device rd;
+  std::mt19937 mersenne(rd());
+
+  train.addCage(mersenne() % 2);
+
+  EXPECT_EQ(1, train.getLenght());
+}
+
+TEST(Train, test4) {
+  Train train;
+  std::random_device rd;
+  std::mt19937 mersenne(rd());
+
+  for (size_t i = 0; i < 2; i++) {
+    train.addCage(mersenne() % 2);
+  }
+
+  EXPECT_EQ(2, train.getLenght());
 }
