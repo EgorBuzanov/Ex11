925b128d54f93c6589166789c32117cc0a8295ba
EgorBuzanov <egor4ek007@gmail.com>
2021-04-05 20:19:47 +0300

    Initial commit

---
+++ include/train.h
@@ -0,0 +1,5 @@
+// Copyright 2021 Egor Buzanov
+#ifndef INCLUDE_TRAIN_H
+#define INCLUDE_TRAIN_H
+
+#endif  // INCLUDE_TRAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
-// Copyright 2021
+// Copyright 2021 Egor Buzanov
 #include <iostream>
 #include <string>
 
 #include "train.h"
 
-int main() {
-  return 0;
-}
-
+int main() { return 0; }
